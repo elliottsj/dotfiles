d841685379f776a2a6e693596a11a7838eb05b25
Spencer Elliott <me@elliottsj.com>
2016-01-31 16:14:21 -0500

    customize colors

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -3,6 +3,9 @@ set fisher_home ~/.fisherman
 set fisher_config ~/.config/fisherman
 source $fisher_home/config.fish
 
+# customize colors
+set --universal fish_color_command '6191fc'
+
 # homeshick
 source "$HOME/.homesick/repos/homeshick/homeshick.fish"
 
